d34852bbf52cd771f544e3b26363bc45ef8c7eda
J-Lee12 <joshualee2022.1@u.northwestern.edu>
2023-01-16 18:50:12 -0600

    finished motors finally this was such a pain...

--- analyze.py
+++ analyze.py
@@ -3,9 +3,16 @@ import matplotlib.pyplot
 
 backLegSensorValues = numpy.load("./data/meow.npy")
 frontLegSensorValues = numpy.load("./data/bark.npy")
-print(backLegSensorValues)
 
-matplotlib.pyplot.plot(backLegSensorValues, label='BackLeg', linewidth=2)
-matplotlib.pyplot.plot(frontLegSensorValues, label='FrontLeg')
-matplotlib.pyplot.legend()
+front = numpy.load("./data/front.npy")
+back = numpy.load("./data/back.npy")
+matplotlib.pyplot.plot(front,linewidth=4)
+matplotlib.pyplot.plot(back)
+
+matplotlib.pyplot.xlabel("Steps")
+matplotlib.pyplot.ylabel("Value in Radians")
+
+# matplotlib.pyplot.plot(backLegSensorValues, label='BackLeg', linewidth=2)
+# matplotlib.pyplot.plot(frontLegSensorValues, label='FrontLeg')
+# matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
--- simulate.py
+++ simulate.py
@@ -12,13 +12,57 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
-backLegSensorValues = numpy.zeros(100)
-frontLegSensorValues = numpy.zeros(100)
-for i in range(0,100):
+frontamplitude = 0
+frontfrequency = 5
+frontphaseOffset = 0
+
+backamplitude = numpy.pi/10
+backfrequency = 8
+backphaseOffset = 0
+
+x = numpy.linspace(0,2*numpy.pi,1000)
+frontLegtargetAngles = frontamplitude * numpy.sin(frontfrequency * x + frontphaseOffset)
+i = 0
+OldRange = (1 - -1)
+NewRange = (numpy.pi/4 - -numpy.pi/4)
+while i < 1000:
+	frontLegtargetAngles[i] = (((frontLegtargetAngles[i] - -1)* NewRange)/ OldRange) + -numpy.pi/4
+	i += 1
+
+y = numpy.linspace(0,2*numpy.pi,1000)
+backLegtargetAngles = backamplitude * numpy.sin(backfrequency * y + backphaseOffset)
+i = 0
+OldRange = (1 - -1)
+NewRange = (numpy.pi/4 - -numpy.pi/4)
+while i < 1000:
+	backLegtargetAngles[i] = (((backLegtargetAngles[i] - -1)* NewRange)/ OldRange) + -numpy.pi/4
+	i += 1
+
+# numpy.save("./data/front.npy", frontLegtargetAngles)
+# numpy.save("./data/back.npy", backLegtargetAngles)
+# quit()
+
+backLegSensorValues = numpy.zeros(1000)
+frontLegSensorValues = numpy.zeros(1000)
+for i in range(0,1000):
 	p.stepSimulation()
 	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+	pyrosim.Set_Motor_For_Joint(
+	bodyIndex = robotId,
+	jointName = 'Torso_BackLeg',
+	controlMode = p.POSITION_CONTROL,
+	targetPosition = backLegtargetAngles[i],
+	maxForce = 500)
+	
+	pyrosim.Set_Motor_For_Joint(
+	bodyIndex = robotId,
+	jointName = 'Torso_FrontLeg',
+	controlMode = p.POSITION_CONTROL,
+	targetPosition = frontLegtargetAngles[i],
+	maxForce = 500)
 	time.sleep(1/60)
 
+	
 numpy.save("./data/bark.npy",frontLegSensorValues)
 numpy.save("./data/meow.npy",backLegSensorValues)
